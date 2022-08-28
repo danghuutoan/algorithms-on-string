7f7b00ff5eeb1e4a2d1538a4a9f9f417793dee52
danghuutoan <danghuutoan2002@gmail.com>
2022-08-28 19:45:15 +0700

    adding solution for problem 2

--- week1/problem2/solution.py
+++ week1/problem2/solution.py
@@ -1,14 +1,6 @@
-# Uses python3
+# python3
+from cgitb import reset
 import sys
-
-# Return the trie built from patterns
-# in the form of a dictionary of dictionaries,
-# e.g. {0:{'A':1,'T':2},1:{'C':3}}
-# where the key of the external dictionary is
-# the node ID (integer), and the internal dictionary
-# contains all the trie edges outgoing from the corresponding
-# node, and the keys are the letters on those edges, and the
-# values are the node IDs to which these edges lead.
 
 
 class Node:
@@ -16,7 +8,7 @@ class Node:
         self.value = ch
         self.node_id = node_id
         self.chidren = dict()
-        self.isEndOfWord = False
+        # self.isEndOfWord = False
 
     def addChild(self, ch: str, node_id: int):
         new_node = Node(ch, node_id)
@@ -28,6 +20,9 @@ class Node:
 
     def hasChild(self, ch: str):
         return ch in self.chidren
+
+    def isEndOfWord(self):
+        return len(self.chidren) < 1
 
 
 class Trie:
@@ -45,6 +40,7 @@ class Trie:
                 self.latest_id = self.latest_id + 1
                 self.updateNodeDict(current_node=current, new_node=new_node)
             current = current.getChild(ch)
+        # current.isEndOfWord = True
 
     def updateNodeDict(self, current_node: Node, new_node: Node):
         if current_node.node_id not in self.nodes:
@@ -52,33 +48,56 @@ class Trie:
 
         self.nodes[current_node.node_id][new_node.value] = new_node.node_id
 
+    def prefixMatch(self, text: str):
+        if len(text) < 1:
+            return False
 
-def build_trie(patterns):
+        s = iter(text)
+        # ch = next(s, None)
+        current = self.root
+
+        while True:
+            ch = next(s, None)
+
+            if current.isEndOfWord() is True:
+                return True
+
+            if ch is None:
+                return False
+
+            if not current.hasChild(ch):
+                return False
+
+            current = current.getChild(ch)
+
+    def match(self, text: str):
+        result = []
+        for i in range(len(text)):
+            if self.prefixMatch(text[i:]):
+                result.append(i)
+
+        return result
+
+
+def solve(text, n, patterns):
     trie = Trie()
+
     for p in patterns:
         trie.insert(p)
-
-    return trie.nodes
-
-
-if __name__ == "__main__":
-    patterns = sys.stdin.read().split()[1:]
-    tree = build_trie(patterns)
-    for node in tree:
-        for c in tree[node]:
-            print("{}->{}:{}".format(node, tree[node][c], c))
     
-    print(tree)
+    result = trie.match(text=text)
+
+    #  write your code here
+
+    return result
 
 
+text = sys.stdin.readline().strip()
+n = int(sys.stdin.readline().strip())
+patterns = []
+for i in range(n):
+    patterns += [sys.stdin.readline().strip()]
 
+ans = solve(text, n, patterns)
 
-# 0->1:A
-# 1->2:T
-# 2->3:A
-# 3->4:G
-# 4->5:A
-# 2->6:C
-# 0->7:G
-# 7->8:A
-# 8->9:T
+sys.stdout.write(" ".join(map(str, ans)) + "\n")
