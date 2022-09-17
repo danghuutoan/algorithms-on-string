8d6b9b42ef3e7faeaa1deac2bb9c25f3cd442ea4
danghuutoan <danghuutoan2002@gmail.com>
2022-09-17 14:26:10 +0700

    update solution for problem2 week 1

--- week1/problem2/solution.py
+++ week1/problem2/solution.py
@@ -88,12 +88,13 @@ def solve(text, n, patterns):
     return result
 
 
-# text = sys.stdin.readline().strip()
-# n = int(sys.stdin.readline().strip())
-# patterns = []
-# for i in range(n):
-#     patterns += [sys.stdin.readline().strip()]
+if __name__ == '__main__':
+    text = sys.stdin.readline().strip()
+    n = int(sys.stdin.readline().strip())
+    patterns = []
+    for i in range(n):
+        patterns += [sys.stdin.readline().strip()]
 
-# ans = solve(text, n, patterns)
+    ans = solve(text, n, patterns)
 
-# sys.stdout.write(" ".join(map(str, ans)) + "\n")
+    sys.stdout.write(" ".join(map(str, ans)) + "\n")
