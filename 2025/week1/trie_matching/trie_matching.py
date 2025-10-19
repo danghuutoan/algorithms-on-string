9cce3b0e1200c1ba5abcd2a3ba096ef1b5d411ed
danghuutoan <danghuutoan2002@gmail.com>
2025-10-19 12:03:42 +0700

    working version

--- 2025/week1/trie_matching/trie_matching.py
+++ 2025/week1/trie_matching/trie_matching.py
@@ -1,25 +1,136 @@
 # python3
 import sys
+from queue import Queue
+from collections import deque
 
 NA = -1
 
+
 class Node:
-	def __init__ (self):
-		self.next = [NA] * 4
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+        self.offset = None
+        self.value = None
 
-def solve (text, n, patterns):
-	result = []
+    def has_edge(self, ch: str):
+        if ch in self.children:
+            return True
 
-	// write your code here
+        return False
 
-	return result
+    def get_child(self, label: str) -> "Node":
+        if label not in self.children:
+            return None
+        return self.children[label]
 
-text = sys.stdin.readline ().strip ()
-n = int (sys.stdin.readline ().strip ())
+    def add_egde(self, label: str) -> "Node":
+        new_node = Node()
+        self.children[label] = new_node
+        return new_node
+
+    def get_children(self) -> dict:
+        return self.children
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str, offset: int = None):
+        current_node = self.root
+
+        for c in word:
+            if current_node.has_edge(c):
+                current_node = current_node.get_child(c)
+            else:
+                current_node = current_node.add_egde(c)
+        current_node.is_end_of_word = True
+        if offset is not None:
+            current_node.offset = offset
+
+    def traverse(self, curr_node: Node = None, prev_label: str = None):
+        if curr_node is None:
+            curr_node = self.root
+        for next_label, next_node in curr_node.get_children().items():
+            self.traverse(next_node, next_label)
+
+    def walkdown(self, curr_node: Node, prev_label: str = None):
+        offset = set()
+        if curr_node is None:
+            curr_node = self.root
+
+        stack = []
+        # queue = deque()
+
+        stack.append(curr_node)
+        # queue.append(curr_node)
+        
+        while len(stack) >0:
+            curr = stack.pop()
+            if curr.is_end_of_word is True:
+                offset.add(curr.offset)
+    
+            for _,next_node in curr.get_children().items():
+                stack.append(next_node)
+                # queue.append(next_node)
+        return offset
+
+    def match(self, pattern: str):
+
+        curr_node = self.root
+        for c in pattern:
+            if curr_node.has_edge(c):
+                curr_node = curr_node.get_child(c)
+            else:
+                return []
+        offset = self.walkdown(curr_node)
+        return offset
+    
+    def suffix_match(self, suffix: str):
+        
+        curr_node = self.root
+
+        
+        for c in suffix:
+            if curr_node.is_end_of_word:
+                return True
+
+            if curr_node.has_edge(c) is False:
+                return False
+
+            curr_node = curr_node.get_child(c)
+
+        if curr_node.is_end_of_word:
+            return True
+        
+        return False
+
+
+def solve(text, n, patterns):
+    result = []
+    trie = Trie()
+    text_len = len(text)
+    
+
+
+    for p in patterns:
+        trie.insert(p)
+
+    for i in range(text_len):
+        suffix = text[i:]
+        if trie.suffix_match(suffix):
+            result.append(i)
+    return result
+
+
+text = sys.stdin.readline().strip()
+n = int(sys.stdin.readline().strip())
 patterns = []
-for i in range (n):
-	patterns += [sys.stdin.readline ().strip ()]
+for i in range(n):
+    patterns += [sys.stdin.readline().strip()]
 
-ans = solve (text, n, patterns)
+ans = solve(text, n, patterns)
 
-sys.stdout.write (' '.join (map (str, ans)) + '\n')
+sys.stdout.write(" ".join(map(str, ans)) + "\n")
