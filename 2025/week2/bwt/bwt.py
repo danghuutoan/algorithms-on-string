34244c49e4091acab06f65f0df7132735fc29b0e
danghuutoan <danghuutoan2002@gmail.com>
2025-11-18 22:55:28 +0700

    add week 2 solution

--- 2025/week2/bwt/bwt.py
+++ 2025/week2/bwt/bwt.py
@@ -2,7 +2,15 @@
 import sys
 
 def BWT(text):
-    return ""
+    n = len(text)
+    arr = []
+    for i in range(n):
+        arr.append(text[i:]+ text[:i])
+    sorted_arr = sorted(arr)
+    bwt = ""
+    for x in sorted_arr:
+        bwt += x[-1]
+    return bwt
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
--- 2025/week2/bwtinverse/bwtinverse.py
+++ 2025/week2/bwtinverse/bwtinverse.py
@@ -1,11 +1,43 @@
 # python3
 import sys
 
+
 def InverseBWT(bwt):
     # write your code here
-    return ""
+    n = len(bwt)
+
+    count_array = []
+    first_column = sorted(bwt)
+    c = {
+        "$": 0,
+        "A": 0,
+        "C": 0,
+        "G": 0,
+        "T": 0,
+    }
+    first_occurence = {}
+    for i in range(n):
+        if first_column[i] not in first_occurence:
+            first_occurence[first_column[i]] = i
+
+        count_array.append(c[bwt[i]])
+        c[bwt[i]] += 1
+
+    pos = 0
+    res = [None] * n
+    res[-1] = "$"
+    for i in range(n - 1):
+        curr_char = bwt[pos]
+        offset = count_array[pos]
+
+        res[n - 2 - i] = curr_char
+        pos = first_occurence[curr_char] + offset
+
+    return "".join(res)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bwt = sys.stdin.readline().strip()
     print(InverseBWT(bwt))
+
+    # $AAAGGG
